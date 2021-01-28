c2050bc3574b112db55e1c65ee645e21a5f14db9
ArtyDy <arthur.devemy@orange.fr>
2021-01-28 17:08:23 +0100

    ud

--- Toolboxes/movingdot.m
+++ Toolboxes/movingdot.m
@@ -3,8 +3,13 @@ sca;
 close all;
 clearvars;
 
+data = load('coords.mat');
+array =data.data;
+
+array = array - array(end);
+
 % Here we call some default settings for setting up Psychtoolbox
-PsychDefaultSetup(2);
+ PsychDefaultSetup(2);
 
 % Get the screen numbers
 screens = Screen('Screens');
@@ -28,6 +33,8 @@ ifi = Screen('GetFlipInterval', window);
 % Get the centre coordinate of the window
 [xCenter, yCenter] = RectCenter(windowRect);
 
+Screen('BlendFunction', window, GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
 % Make a base Rect of 200 by 200 pixels
 baseRect = [0 0 200 200];
 
@@ -37,10 +44,13 @@ rectColor = [1 0 0];
 % Our square will oscilate with a sine wave function to the left and right
 % of the screen. These are the parameters for the sine wave
 % See: http://en.wikipedia.org/wiki/Sine_wave
-amplitude = screenXpixels * 0.25;
-frequency = 0.2;
-angFreq = 2 * pi * frequency;
-startPhase = 0;
+
+ 
+
+
+% frequency = 0.2;
+% angFreq = 2 * pi * frequency;
+% startPhase = 0;
 time = 0;
 
 % Sync us and get a time stamp
@@ -50,30 +60,61 @@ waitframes = 1;
 % Maximum priority level
 topPriorityLevel = MaxPriority(window);
 Priority(topPriorityLevel);
+endpos = 0.75*screenYpixels;
+startpos = 0.25*screenYpixels;
 
+Screen('DrawDots', window, [xCenter startpos], 100, [1 1  1], [], 3);
+vbl = Screen('Flip', window); 
+
+
+ KbPressWait;
+ WaitSecs(0.5);
+ 
+
+ 
+ endreach =0;
+ 
+ 
+ squareYpos=startpos; 
 % Loop the animation until a key is pressed
-while ~KbCheck
+while ~KbCheck & ~endreach
+    
+    amplitude = screenYpixels *0.01*(yCenter/(yCenter + 4*abs(squareYpos-yCenter))) ;
 
-    % Position of the square on this frame
-    xpos = amplitude * sin(angFreq * time + startPhase);
+    % Position of the square on t his frame
+    ypos =  amplitude;
 
     % Add this position to the screen center coordinate. This is the point
     % we want our square to oscillate around
-    squareXpos = xCenter + xpos;
+    squareYpos = squareYpos + ypos;
+    
+    if squareYpos >= endpos
+        endreach = 1;
+    end
+    
 
     % Center the rectangle on the cent re of the screen
-    centeredRect = CenterRectOnPointd(baseRect, squareXpos, yCenter);
+    centeredRect = CenterRectOnPointd(baseRect, xCenter, squareYpos);
 
     % Draw the rect to the screen
-    Screen('DrawDots', window, [squareXpos yCenter], 20, [1 0  0], [], 2);
-
-    % Flip to the screen
-    vbl  = Screen('Flip', window, vbl + (waitframes - 0.5) * ifi);
-
-    % Increment the time
-     time = time + ifi;
+    Screen('DrawDots', window, [xCenter squareYpos], 100, [1 1  1], [], 3);
+    
+      % Flip to the screen
+    vbl  = Screen('Flip', window, vbl + (waitframes - 0.5) * ifi); 
+     
+    % Increment the time 
+    time = time + ifi;
+    
+    
+     
+  
 
 end
+
+Screen('DrawDots', window, [xCenter endpos], 100, [1 1  1 ], [], 3    );
+vbl = Screen('Flip', window); 
+
+KbPressWait;
 
 % Clear the screen
 sca;
